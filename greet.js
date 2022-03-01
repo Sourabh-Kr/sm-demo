042212c5ff6015d874e5d20a93afbaceedde4cd6
Sourabh-Kumar <sourabh.kr001@paytm.com>
2022-03-01 21:34:54 +0530

    greet log 2

--- greet.js
+++ greet.js
@@ -1,4 +1,5 @@
 console.log('Welcome to Sub Module SK')
 console.log('the push from dirctly Submodule Repo')
 console.log('the push-2 from dirctly Submodule Repo')
+console.log('the push-3 from dirctly Submodule Repo')
 console.log('the End of Sub Module from SK')
