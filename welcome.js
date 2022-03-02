741c551a66297406bad24445ccce35501185fb5a
Sourabh-Kumar <sourabh.kr001@paytm.com>
2022-03-02 22:14:25 +0530

    Squashed 'subtreeDirectory/' changes from 1dc0660..77e5c62
    77e5c62 added 2nd console log welco2
    e1a62c1 new file added
    c437ffb push to subtree from parent
    
    git-subtree-dir: subtreeDirectory
    git-subtree-split: 77e5c6257fcfdfcfac5d3ab0a525d3af05f35086

--- welcome.js
+++ welcome.js
@@ -1,3 +1,1 @@
-console.log('hello direct push from s-mod-2 by sk')
-console.log('hello direct push$$$$2 from s-mod-2 by sk')
-console.log('hello direct push-3 from s-mod-2 by sk')
+console.log('hello  push from parent to subtree by sk')
